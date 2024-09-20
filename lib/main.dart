0969c43e44526a8eb4bb16447c48ba24d5f03d84
burillu <burillu90@hotmail.it>
2024-09-20 10:17:20 +0200

    firebase plugin ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:fimber/fimber.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:telegram_app/app.dart';
+import 'package:telegram_app/firebase_options.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(); //);
+
   Fimber.plantTree(DebugTree());
   runApp(App());
 }
