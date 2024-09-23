405e18e93e6edc26895ab26541524d2b8770e979
burillu <burillu90@hotmail.it>
2024-09-23 15:37:55 +0200

    add auto router, create first build

---
+++ lib/router/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:telegram_app/app.dart';
+import 'package:telegram_app/router/app_router.gr.dart';
+
+// @MaterialAutoRouter(
+//   replaceInRouteName: 'Page,Route',
+//   preferRelativeImports: false,
+//   routes: <AutoRoute>[
+//     AutoRoute(page: MainPage, initial: true),
+//   ],
+// )
+// class $AppRouter {}
+
+@AutoRouterConfig()
+class AppRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+        /// routes go here
+        AutoRoute(page: MainRoute.page, initial: true),
+      ];
+}
