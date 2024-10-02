9597edfa78a2a76efe672b6343eb989854674804
burillu <burillu90@hotmail.it>
2024-10-02 19:25:01 +0200

    more dependency and init

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:telegram_app/app.dart';
 import 'package:telegram_app/firebase_options.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,5 +11,7 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
 
   Fimber.plantTree(DebugTree());
+  timeago.setLocaleMessages('it', timeago.ItMessages());
+  timeago.setLocaleMessages('en', timeago.EnMessages());
   runApp(App());
 }
