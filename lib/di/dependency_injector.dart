c1b8f68fb150db2cfce30a4e0e68f708e5b8e772
burillu <burillu90@hotmail.it>
2024-10-02 16:26:00 +0200

    firebasestore dependency and injection

--- lib/di/dependency_injector.dart
+++ lib/di/dependency_injector.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -36,6 +37,9 @@ class DependencyInjector extends StatelessWidget {
               'https://www.googleapis.com/auth/userinfo.profile',
             ]),
           ),
+          Provider<FirebaseFirestore>(
+            create: (_) => FirebaseFirestore.instance,
+          )
         ],
         child: child,
       );
