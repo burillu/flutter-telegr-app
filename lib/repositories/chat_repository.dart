3dbadc2f9b4dc31cc8ab49cc7c750f6f9755ca9b
burillu <burillu90@hotmail.it>
2024-10-09 17:16:10 +0200

    improve chatrepository getter!!

--- lib/repositories/chat_repository.dart
+++ lib/repositories/chat_repository.dart
@@ -1,10 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:telegram_app/exceptions/chat_repository_exception.dart';
 import 'package:telegram_app/extension/fututre_map.dart';
-import 'package:telegram_app/misc/mappers/firebase_chat_mapper.dart';
 import 'package:telegram_app/misc/mappers/firebase_mapper.dart';
-import 'package:telegram_app/misc/mappers/firebase_user_mapper.dart';
 import 'package:telegram_app/models/chat.dart';
 import 'package:telegram_app/models/user.dart';
 
@@ -18,62 +15,32 @@ class ChatRepository {
       required this.firebaseChatMapper,
       required this.firebaseFirestore});
 
-  staticChats(String uid) {
-    final chatsList = firebaseFirestore.collection("chats").get().then(
-      (querySnapshot) {
-        print("Successfully completed");
-
-        final List<Chat> chats = [];
-        for (var docSnapshot in querySnapshot.docs) {
-          chats.add(firebaseChatMapper.fromFirebase(docSnapshot.data()));
-          print('${docSnapshot.id} => ${docSnapshot.data()}');
-        }
-        return chats;
-      },
-      onError: (e) => print("Error completing: $e"),
-    );
-  }
-
-  // .where('users',
-  //     arrayContainsAny: [firebaseFirestore.collection('users').doc(uid)])
-  // .snapshots()
-  // .asyncMap(
-  //   (snapshot) => snapshot.docs.map<Chat>(
-  //     (chatSnapshot) {
-  //       final chat = firebaseChatMapper.fromFirebase(chatSnapshot.data());
-  //       return chat;
-  //     },
-  //   ).toList(),
-  // );
-
   Stream<List<Chat>> chats(String uid) => firebaseFirestore
-          .collection("chats")
-          // .where('users',
-          //     arrayContainsAny: [firebaseFirestore.collection('users').doc(uid)])
-          .snapshots()
-          .asyncMap(
+      .collection("chats")
+      .where('users',
+          arrayContainsAny: [firebaseFirestore.collection('users').doc(uid)])
+      .snapshots()
+      .asyncMap(
         (snapshot) {
           // print(snapshot.docs);
           return snapshot.docs.futureMap<Chat>(
             (chatSnapshot) async {
               final chat = firebaseChatMapper.fromFirebase(chatSnapshot.data());
 
-              final userReference = (chatSnapshot.data()['users'] as List)
-                  .firstWhere((userReference) => userReference.id != uid,
-                      orElse: () => null);
-              // print("cerco lo snapshot");
+              final userReference =
+                  (chatSnapshot.data()['users'] as List).firstWhere((user) {
+                return user.id != uid;
+              }, orElse: () => null);
               if (userReference == null) {
-                //   print("snapshot non Trovato!");
                 throw ChatRepositoryException();
               }
 
               final userSnapshot = await userReference.get();
-              // final user = firebaseUserMapper
-              //     .fromFirebase(userSnapshot.data())
-              // .copyWith(id: userSnapshot.id);
-              // if (chat.id == null) throw ChatRepositoryException();
+              final user = firebaseUserMapper
+                  .fromFirebase(userSnapshot.data())
+                  .copyWith(id: userSnapshot.id);
 
-              return chat.copyWith(id: chatSnapshot.id);
+              return chat.copyWith(user: user, id: chatSnapshot.id);
             },
           );
         },
